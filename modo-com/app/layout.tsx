a07b8c4a93d361082f63d7cd486d0e1f542dd927
gongahkia <angryapplegravy@gmail.com>
2025-02-09 16:28:52 +0800

    feat(app): thas fine gd

--- modo-com/app/layout.tsx
+++ modo-com/app/layout.tsx
@@ -6,7 +6,7 @@ import type React from "react" // Import React
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Modo",
+  title: "The Modo Collective",
   description: "Modo is a social media platform designed for artists, by artists. Stop scrolling, start creating.",
 }
 
